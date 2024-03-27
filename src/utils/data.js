2838269e69e97b08ac6e90a44f1abfa29541c599
Azarul Islam <azarul81@gmail.com>
2024-03-27 10:51:54 +0530

    Add files via upload

---
+++ src/utils/data.js
@@ -0,0 +1,156 @@
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { CiMobile1 } from "react-icons/ci";
+import { MdWorkspacesOutline } from "react-icons/md";
+export const projectExperience = [
+  {
+    name: "Website Design",
+    projects: 76,
+    icon: HiOutlineDesktopComputer,
+    bg: "#286F6C",
+  },
+  {
+    name: "Mobile App Design",
+    projects: 63,
+    icon: CiMobile1,
+    bg: "#EEC048",
+  },
+  {
+    name: "Brand Identity",
+    projects: 47,
+    icon: MdWorkspacesOutline,
+    bg: "#F26440",
+  },
+];
+
+export const WhatDoIHelp = [
+  "I will help you with finging a solution and solve your problem, We use process design to create digital products. Besides that also help their business.",
+  "We use process design to create digital products. Besides that also help their business",
+];
+
+export const workExp = [
+  {
+    place: "Self-Employed, Brisbane",
+    tenure: "Aug 2014 - Sep 2016",
+    role: "Visual Designer",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+  {
+    place: "New Man Services",
+    tenure: "Aug 2014 - Sep 2016",
+    role: "UI/UX Designer",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+  {
+    place: "Global Solution",
+    tenure: "Aug 2014 - Sep 2016",
+    role: "Sr. Product Designer",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+];
+
+export const comments = [
+  {
+    name: "M V O",
+    post: "Visitante",
+    comment:
+      "He probado varios psicólogos en mi vida, porque he vivido en varios sitios, pues Clara es lo mejor que me ha pasado nunca. Además me ha tocado en la etapa que empecé a tener niños y me ha ayudado infinito con todos los procesos, tiene una empatía y una forma de ayudar muy especial a la vez que realista! En fin, que os la recomiendo muchísimo y no solo para adultos, para niños también. Gracias Clara! Nos vemos pronto ;)",
+    img: "./people2.png",
+  },
+  {
+    name: "Aroa",
+    post: "Visitante",
+    comment:
+      "Clara me ayudo muchísimo a llevar un proceso muy duro. Sabe escuchar y comunicarse. A mi personalmente me llevo a mi perdón y a ver las cosas desde otro punto de vista. Gracias Clara.",
+    img: "./people1.png",
+  },
+  {
+    name: "Ana Cano",
+    post: "Visitante",
+    comment:
+      "Ana Cano recomienda a Clara López, su psicóloga, con la que lleva trabajando un tiempo. Dice que su experiencia ha sido increíblemente positiva, que Clara tiene una empatía y una forma de ayudar muy especial. La recomienda al 100% y la agradece por todo lo que ha hecho por ella.",
+    img: "./people2.png",
+  },
+  // {
+  //   name: "Anamika Sandula",
+  //   post: "Creative Manager",
+  //   comment:
+  //     "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  //   img: "./people1.png",
+  // },
+  // {
+  //   name: "Anamika Sandula",
+  //   post: "Creative Manager",
+  //   comment:
+  //     "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  //   img: "./people2.png",
+  // },
+  // {
+  //   name: "Anamika Sandula",
+  //   post: "Creative Manager",
+  //   comment:
+  //     "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  //   img: "./people1.png",
+  // },
+  // {
+  //   name: "Anamika Sandula",
+  //   post: "Creative Manager",
+  //   comment:
+  //     "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  //   img: "./people2.png",
+  // },
+  // {
+  //   name: "Anamika Sandula",
+  //   post: "Creative Manager",
+  //   comment:
+  //     "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  //   img: "./people1.png",
+  // },
+  // {
+  //   name: "Anamika Sandula",
+  //   post: "Creative Manager",
+  //   comment:
+  //     "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  //   img: "./people2.png",
+  // },
+];
+
+export const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  touchMove: true,
+  useCSS: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
